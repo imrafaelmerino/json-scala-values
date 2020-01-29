b02966d477420e2425089fcb59b360561fe59959
imrafaelmerino <imrafael.merino@gmail.com>
2020-01-29 18:35:15 +0100

    test: 💍 more tests

--- src/test/scala/value/JsArraySpecification.scala
+++ src/test/scala/value/JsArraySpecification.scala
@@ -1,15 +1,23 @@
 package value
 
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
+import value.JsObjSpecification.property
 import valuegen.{RandomJsArrayGen, RandomJsObjGen}
 
 object JsArraySpecification extends Properties("JsArray") {
 
 
   property("if two arrays are equals, they have the same hashcode") =
-    forAll(RandomJsArrayGen()) { (a: JsArray) =>
-    val either = JsArrayParser.parse(a.toString)
-    either.contains(a) && either.exists(_.hashCode() == a.hashCode())
+    forAll(RandomJsArrayGen()) { (x: JsArray) =>
+    val either = JsArrayParser.parse(x.toString)
+    either.contains(x) && either.exists(_.hashCode() == x.hashCode())
   }
+
+  property("an array is just a set of path/value pairs") =
+    forAll(RandomJsArrayGen(arrLengthGen = Gen.choose(1,20))){
+      (x:JsArray) =>
+        val pairs = x.flatten
+        x == JsArray(pairs.head,pairs.tail: _*)
+    }
 }
--- src/test/scala/value/JsObjSpecification.scala
+++ src/test/scala/value/JsObjSpecification.scala
@@ -1,16 +1,22 @@
 package value
 
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 import valuegen.RandomJsObjGen
 
 object JsObjSpecification extends Properties("JsObj") {
 
 
   property("if two object are equals, they have the same hashcode") =
-    forAll(RandomJsObjGen()) { (a: JsObj) =>
-    val either = JsObjParser.parse(a.toString)
-    either.contains(a) && either.exists(_.hashCode() == a.hashCode())
+    forAll(RandomJsObjGen()) { (x: JsObj) =>
+    val either = JsObjParser.parse(x.toString)
+    either.contains(x) && either.exists(_.hashCode() == x.hashCode())
   }
+
+  property("an object is just a set of path/value pairs") =
+    forAll(RandomJsObjGen(objSizeGen = Gen.choose(1,20))){
+      (x:JsObj) =>
+        x == JsObj(x.flatten: _*)
+    }
 
 }

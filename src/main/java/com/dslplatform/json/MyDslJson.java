57350b0e07563b36da6778cfe5541af78b792a91
imrafaelmerino <imrafael.merino@gmail.com>
2020-03-15 02:47:40 +0100

    feat: 🎸 close #63

--- src/main/java/com/dslplatform/json/MyDslJson.java
+++ src/main/java/com/dslplatform/json/MyDslJson.java
@@ -1,5 +1,8 @@
 package com.dslplatform.json;
 
+import com.dslplatform.json.serializers.JsArraySerializer;
+import com.dslplatform.json.serializers.JsObjSerializer;
+import com.dslplatform.json.serializers.JsValueSerializer;
 import value.JsArray;
 import value.JsObj;
 import value.JsValue;
@@ -10,6 +13,22 @@ import java.util.function.Function;
 
 public final class MyDslJson<Object> extends DslJson<Object>
 {
+
+
+   public static MyDslJson<?> buildDslJson()  {
+      MyDslJson<?> dslJson = new  MyDslJson<>();
+      JsValueSerializer valueSerializer = new JsValueSerializer();
+      JsonWriter.WriteObject<JsObj> objSerializer = new JsObjSerializer(valueSerializer);
+      JsonWriter.WriteObject<JsArray> arraySerializer = new JsArraySerializer(valueSerializer);
+
+      valueSerializer.setArraySerializer (arraySerializer);
+      valueSerializer.setObjectSerializer (objSerializer);
+
+      dslJson.registerWriter (JsObj.class, objSerializer);
+
+      dslJson.registerWriter(JsArray.class, arraySerializer);
+      return dslJson;
+     }
 
     private JsonReader getReader(final byte[] bytes
                                 )
